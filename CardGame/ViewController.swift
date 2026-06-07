d927e52b1232655e8070d912f12435c42f44705b
tomer <tomerfeldon1@gmail.com>
2026-06-07 17:11:20 +0300

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Student22 on 07/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CardGameTests/CardGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameTests.swift
+//  CardGameTests
+//
+//  Created by Student22 on 07/06/2026.
+//
+
+import Testing
+@testable import CardGame
+
+struct CardGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
